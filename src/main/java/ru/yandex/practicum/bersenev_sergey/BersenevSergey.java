aa6e0d19c38aaa2a94dea30cbfd81c3edf8d4e01
Dreamlike <sbersenev@yandex.ru>
2025-12-13 23:16:19 +0300

    Проверка работоспособности всех систем. Добавил первый класс.

---
+++ src/main/java/ru/yandex/practicum/bersenev_sergey/BersenevSergey.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.bersenev_sergey;
+
+public class BersenevSergey {
+    public static void main(String[] args) {
+        System.out.println("Привет от Сергея Берсенева!");
+    }
+}
