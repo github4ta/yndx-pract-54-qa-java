f15c06501af3dc059d2cd50ea3ea781084ab01b9
mrironheart-kiu <mrironheart@gmail.com>
2025-12-15 22:02:14 +0300

    Onboarding hello from Ивана Костина

---
+++ src/main/java/ru/yandex/practicum/kostin_ivan/KostinIvan.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.kostin_ivan;
+
+public class KostinIvan {
+    public static void main(String[] args) {
+        System.out.println("Привет от Ивана Костина!");
+    }
+}
