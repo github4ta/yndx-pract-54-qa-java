21295a5af13365d6f00fdd562bc369354704bde4
Alexandr Romaniuc <alexromaniuc@gmail.com>
2025-12-16 21:42:59 +0200

    Add class Hello

---
+++ src/main/java/ru/yandex/practicum/romaniuc_alexandr/Hello.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.romaniuc_alexandr;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello from Alexandr Romaniuc");
+    }
+}
