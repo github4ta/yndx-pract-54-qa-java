1baf537af03ae235be56ccdcfe8a5e34526f6a6d
Alexandr Romaniuc <alexromaniuc@gmail.com>
2025-12-22 21:44:30 +0200

    add comment

--- src/main/java/ru/yandex/practicum/romaniuc_alexandr/Hello.java
+++ src/main/java/ru/yandex/practicum/romaniuc_alexandr/Hello.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.romaniuc_alexandr;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello from Alexandr Romaniuc");//
+        System.out.println("Hello from Alexandr Romaniuc");//add comment
     }
 }
