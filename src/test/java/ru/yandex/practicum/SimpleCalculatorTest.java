d200ee0d8d3c4e019aec30a00e2dc7a5a688e246
Kostin Ivan <mrironheart@gmail.com>
2026-01-11 14:36:26 +0300

    Реализовал тест по задаче CALC_DEV-0012

--- src/test/java/ru/yandex/practicum/SimpleCalculatorTest.java
+++ src/test/java/ru/yandex/practicum/SimpleCalculatorTest.java
@@ -1,4 +1,31 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SimpleCalculatorTest {
+    private SimpleCalculator simpleCalculator;
+
+    @BeforeEach
+    void init(){
+        simpleCalculator = new SimpleCalculator();
+    }
+
+    @Test
+    @DisplayName(
+            "При вызове метода SimpleCalculator.subtract(int a, int b), " +
+                    "где a = 10_000 и b = 100," +
+                    "метод возвращает результат равный 9_900")
+    void subtract() {
+        int a = 10_000;
+        int b = 100;
+        int expectedResult = 9_900;
+
+        int actualResult = simpleCalculator.subtract(a,b);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
