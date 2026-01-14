7dff747b1d432fb555cff6b5de158dece0fd7438
Sergei Tsarik <49017393+github4ta@users.noreply.github.com>
2026-01-14 21:14:31 +0300

    CALC_TEST-0015 – Калькулятор – Тестировать метод умножать(double a, double b) для положительных чисел

--- src/test/java/ru/yandex/practicum/SimpleCalculatorTest.java
+++ src/test/java/ru/yandex/practicum/SimpleCalculatorTest.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class SimpleCalculatorTest {
+
+    @Test
+    @DisplayName("Тестировать метод умножать(double a, double b) для положительных чисел a = 100.0, b = 10.0 когда ожидаемый результат 1000.0")
+    public void testMultiplyWithPositiveNumbersInDoubleThatReturnsResultInDouble() {
+        Assertions.assertEquals(1000.0, new SimpleCalculator().multiply(100.0, 10.0));
+    }
 }
